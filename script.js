1206a6ae54ecf2383e1222ae2e373e6a2d6c938a
Nehemiah Kemayah <81781111+Ingenious21@users.noreply.github.com>
2023-07-31 21:41:51 +0000

    Simple Interest Calculator Initial Code

---
+++ script.js
@@ -0,0 +1,17 @@
+function compute() {
+    // Get the principal amount, rate, and number of years from the inputs
+    const principal = parseFloat(document.getElementById("principal").value);
+    const rate = parseFloat(document.getElementById("rate").value);
+    const years = parseInt(document.getElementById("years").value);
+
+    // Calculate simple interest
+    const interest = (principal * rate * years) / 100;
+
+    // Display the result
+    document.getElementById("result").textContent = interest.toFixed(2);
+}
+
+// Update the displayed rate value when the range input changes
+document.getElementById("rate").addEventListener("input", function() {
+    document.getElementById("rate_val").textContent = this.value;
+});
