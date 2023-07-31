82ee20cd1e332f453778a62f7e588122bd64ccb5
Nehemiah Kemayah <81781111+Ingenious21@users.noreply.github.com>
2023-07-31 22:09:05 +0000

    Bug Fixes

--- script.js
+++ script.js
@@ -8,13 +8,10 @@ function compute() {
     const interest = (principal * rate * years) / 100;
 
     // Display the result
-    document.getElementById("result").textContent = "Interest: " + interest.toFixed(2);
+    document.getElementById("result").textContent = interest.toFixed(2);
 }
 
 // Update the displayed rate value when the range input changes
 document.getElementById("rate").addEventListener("input", function() {
     document.getElementById("rate_val").textContent = this.value;
 });
-
-// Call compute() initially to display the initial interest value
-compute();
